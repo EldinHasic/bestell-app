fefc903baae559d277ecef257bfa5ec282234520
EldinHasic <eldinlejla287@gmail.com>
2026-02-08 11:17:08 +0100

    completed template burger function

--- scripts/db.js
+++ scripts/db.js
@@ -1,21 +1,25 @@
 let MyMenu = {
   "Burger": [
     {
+      "image": "./assets/img/veggie-burger.jpg",
       "name": "Veggie mushroom black burger",
       "price": 16.90,
       "ingredients": "Mixed green salad, Tomatoes, Edamame, Mushrooms"
     },
     {
+      "image": "./assets/img/all-meat-burger.jpg",
       "name": "All meat burger",
       "price": 15.90,
       "ingredients": "Beef, Bacon, Dill, pickels, Smoked cheese, Ketchup, BBQ sauce"
     },
     {
+      "image": "./assets/img/beef-red-burger.jpg",
       "name": "Beef red burger",
       "price": 14.90,
       "ingredients": "Beef, Cheese, Tomatoes, Lettuce, Onion"
     },
     {
+      "image": "./assets/img/big-chicken-burger.jpg",
       "name": "Big chicken burger",
       "price": 15.90,
       "ingredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell peeper"
--- scripts/template.js
+++ scripts/template.js
@@ -1,7 +1,28 @@
- function renderBurgerDishes() {
+function renderBurgerDishes() {
     let BurgerSectionRef = document.getElementById("burgerMenu");
     for (let BurgerIndex = 0; BurgerIndex < MyMenu.Burger.length; BurgerIndex++) {
         BurgerSectionRef.innerHTML += getdishHTML(BurgerIndex);
     }
- }  
- 
+}
+
+function getdishHTML(BurgerIndex) {
+return `<div class=" contentLimit dishContainer">
+    <div>
+        <img class="dish-img" src="${MyMenu.Burger[BurgerIndex].image}" alt="">
+    </div>
+
+    <div class="dishInfo">
+        <div class="dishTitleAndPrice">
+            <h3>${MyMenu.Burger[BurgerIndex].name}</h3>
+            <p>${MyMenu.Burger[BurgerIndex].price.toFixed(2)}€</p>
+        </div>
+        <div>
+            <p>${MyMenu.Burger[BurgerIndex].ingredients}</p>
+        </div>
+        <div class="addToBasketButtons">
+            <img src="./assets/img/added-button.svg" alt="">
+            <img src="./assets/img/add-dish-button.svg" alt="">
+        </div>
+    </div>
+</div>`
+}
